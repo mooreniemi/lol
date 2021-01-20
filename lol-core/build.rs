e23bfac2e8c8b253927b6b1b28908a96b32fa227
Akira Hayakawa <ruby.wktk@gmail.com>
2021-01-20 08:26:41 +0000

    Use prost as the internal serializer

--- lol-core/build.rs
+++ lol-core/build.rs
@@ -10,5 +10,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .compile_with_config(config, &["proto/lol-core.proto"], &["proto"])?;
 
+    let mut config = prost_build::Config::new();
+    config.bytes(&[".lol_core_internal.Req.message"]);
+    config.compile_protos(&["proto/lol-core-internal.proto"], &["proto"])?;
+
+    let OUT_DIR = "proto_generated";
+    std::fs::create_dir(OUT_DIR);
+    let mut config = prost_build::Config::new();
+    config.bytes(&[".lol_core_internal.Req.message"]);
+    config.out_dir(OUT_DIR);
+    config.compile_protos(&["proto/lol-core-internal.proto"], &["proto"])?;
+
     Ok(())
 }
